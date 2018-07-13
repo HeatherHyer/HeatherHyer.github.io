e0ba849a745390cf8690e65befffba2db37037af
HeatherHyer <heather.hyer@familysearch.org>
2018-07-13 14:11:56 -0600

    Add files via upload

---
+++ dgm2760/01/js/main.js
@@ -0,0 +1,15 @@
+/*jslint browser:true*/
+"use strict";
+
+var company = "Joe's Bed and Breakfast";
+document.getElementById("company").innerHTML = company;
+
+var slogan = "Document Writing";
+document.getElementById("slogan").innerHTML = slogan;
+
+var person = prompt("Please enter your first name:", "Your name here...");
+person = " " + person;
+document.getElementById("welcome").innerHTML = "Hi" + person + ". Welcome to Joe's";
+
+var date = new Date();
+document.getElementById("date").innerHTML = "Today is " + date;
