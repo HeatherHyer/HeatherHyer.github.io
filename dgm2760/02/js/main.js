da3a075a3af3e906fdf305d0b63518949d9f40a5
HeatherHyer <heather.hyer@familysearch.org>
2018-07-16 16:08:39 -0600

    getOffset

--- dgm2760/02/js/main.js
+++ dgm2760/02/js/main.js
@@ -5,8 +5,15 @@
 function getWindowSize() {
   var width = window.innerWidth;
   var height = window.innerHeight;
-  var text = "Window size is " + width + " wide by " + height + " tall";
+  var text = "Window size is " + width + " wide by " + height + " tall.";
   document.getElementById("windowSize").innerHTML = text;
 }
-
 getWindowSize();
+
+function getOffset() {
+  var left = window.pageXOffset;
+  var top = window.pageYOffset;
+  var text = "Window offset is " + left + " from the left edge and " + top + " from the top of the display."
+  document.getElementById("windowOffset").innerHTML = text;
+}
+getOffset();
