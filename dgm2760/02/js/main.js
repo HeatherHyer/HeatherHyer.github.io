37222008fad771bc10f39239435122af75ff950e
HeatherHyer <heather.hyer@familysearch.org>
2018-07-16 12:31:33 -0600

    windowSize

--- dgm2760/02/js/main.js
+++ dgm2760/02/js/main.js
@@ -2,3 +2,9 @@
 /*jslint browser:true */
 "use strict";
 
+function getWindowSize() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var text = "Window size is " + width + " wide by " + height + " tall";
+  document.getElementById("windowSize").innerHTML = text;
+}
