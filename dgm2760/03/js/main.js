e1e3833560d3a80ec16b75b3a329b483f2c26455
HeatherHyer <heather.hyer@familysearch.org>
2018-07-23 16:24:22 -0600

    initial js

--- dgm2760/03/js/main.js
+++ dgm2760/03/js/main.js
@@ -2,3 +2,18 @@
 /*jslint browser:true */
 "use strict";
 
+function getRandomNum(low, high) {
+  return Math.floor((Math.random() * high) + low);
+}
+
+var correct = getRandomNum(1, 15);
+var guess;
+var count = 0;
+
+function checkGuess(num) {
+  count++;
+  if (num < correct) {return "Your guess was too low.";}
+  else if (num > correct) {return "Your guess was too high.";}
+  else if (num === correct) {return "You are correct!";}
+  else {return "Your guess was out of range. Remember to pick a number between 1 and 15."}
+}
