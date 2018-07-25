fc0f8a0ea3c9244b0cbeccde772a93181337fb2e
HeatherHyer <heather.hyer@familysearch.org>
2018-07-24 23:10:41 -0600

    moved switch statement to function

--- dgm2760/03/js/main.js
+++ dgm2760/03/js/main.js
@@ -3,7 +3,7 @@
 "use strict";
 
 var count = 0;
-var score;
+var score = 0;
 var pathToImg;
 var awardImg = document.createElement("IMG");
 var correct = Math.floor((Math.random() * 15) + 1);
@@ -17,25 +17,24 @@ function checkGuess() {
   else if (num == correct) {
     score = count;
     document.getElementById("feedback").innerHTML = "You are correct!";
+      switch (score) {
+        case 1:
+        case 2:
+        case 3:
+          pathToImg = "images/blueRibbon.png";
+          break;
+        case 4:
+        case 5:
+        case 6:
+          pathToImg = "images/redRibbon.png";
+          break;
+      }
+      if (score > 6) {
+        pathToImg = "images/yellowRibbon.png";
+      }
+
+      awardImg.setAttribute('src', pathToImg);
+      document.getElementById('award').appendChild(awardImg);
   }
   else {document.getElementById("feedback").innerHTML = "Your guess was out of range. Remember to pick a number between 1 and 15."}
 }
-
-switch (score) {
-  case 1:
-  case 2:
-  case 3:
-    pathToImg = "images/blueRibbon.png";
-    break;
-  case 4:
-  case 5:
-  case 6:
-    pathToImg = "images/redRibbon.png";
-    break;
-}
-if (score >6) {
-  pathToImg = "images/yellowRibbon.png";
-}
-
-awardImg.setAttribute('src', pathToImg);
-document.getElementById('award').appendChild(awardImg);
