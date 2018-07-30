2d6b7ce885b13e49c4a817da888bbebc095204be
HeatherHyer <heather.hyer@familysearch.org>
2018-07-30 13:59:25 -0600

    showThird and showFourth functions

--- dgm2760/05/js/main.js
+++ dgm2760/05/js/main.js
@@ -3,11 +3,36 @@
 "use strict";
 
 var trees = ["Oak", "Apple", "Pine"];
-displayTrees();
 
-function displayTrees() {
+var displayTrees = function() {
   for (var i = 0; i < trees.length; i++) {
     document.getElementById("treeList").innerHTML += trees[i] + "<br>";
   }
   document.getElementById("treeLength").innerHTML += "~The tree list is " + trees.length + " elements long~";
-}
+};
+
+var showThird = function() {
+  if (trees.length < 3) {
+    document.getElementById("error").innerHTML = "There isn't a third tree in your list to display. Try adding more first.";
+  }
+  else {
+    document.getElementById("error").innerHTML = trees[2];
+    displayTrees;
+  }
+};
+
+var showThirdBtn = document.getElementById("showThird");
+showThirdBtn.onclick = showThird;
+
+var showFourth = function() {
+  if (trees.length < 4) {
+    document.getElementById("error").innerHTML = "There isn't a fourth tree in your list to display. Try adding more first.";
+  }
+  else {
+    document.getElementById("error").innerHTML = trees[3];
+    displayTrees;
+  }
+};
+
+var showFourthBtn = document.getElementById("showFourth");
+showFourthBtn.onclick = showFourth;
