8f65fb570e673ea8e97fc3fa74316fec5f45048d
HeatherHyer <heather.hyer@familysearch.org>
2018-07-30 08:40:33 -0600

    barebones js functions

--- dgm2760/04/js/main.js
+++ dgm2760/04/js/main.js
@@ -2,3 +2,18 @@
 /*jslint browser:true */
 "use strict";
 
+function getRandomInt(high) {
+  return Math.floor((Math.random() * high) + 1);
+}
+
+funtion getRandomMonth() {
+
+}
+
+funtion getRandomDay() {
+
+}
+
+function getRandomFortune() {
+  
+}
