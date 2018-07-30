59b4950bf3499ff7d8f5273901c4912879be1701
HeatherHyer <heather.hyer@familysearch.org>
2018-07-30 13:27:44 -0600

    display initial tree list

--- dgm2760/05/js/main.js
+++ dgm2760/05/js/main.js
@@ -2,3 +2,12 @@
 /*jslint browser:true */
 "use strict";
 
+var trees = ["Oak", "Apple", "Pine"];
+displayTrees();
+
+function displayTrees() {
+  for (var i = 0; i < trees.length; i++) {
+    document.getElementById("treeList").innerHTML += trees[i] + "<br>";
+  }
+  document.getElementById("treeLength").innerHTML += "~The tree list is " + trees.length + " elements long~";
+}
