ba82a732ca49c4d2533dcbebd281367cc9e03bde
HeatherHyer <heather.hyer@familysearch.org>
2018-07-30 14:39:53 -0600

    remove... functions

--- dgm2760/05/js/main.js
+++ dgm2760/05/js/main.js
@@ -12,6 +12,7 @@ function displayTrees() {
 var addBirch = function() {
   trees.push("Birch");
   displayTrees();
+  document.getElementById("error").innerHTML = "";
 }
 var addBirchBtn = document.getElementById("addBirch");
 addBirchBtn.onclick = addBirch;
@@ -19,9 +20,49 @@ addBirchBtn.onclick = addBirch;
 var addPeach  = function() {
   trees.unshift("Peach");
   displayTrees();
+  document.getElementById("error").innerHTML = "";
 };
 var addPeachBtn = document.getElementById("addPeach");
 addPeachBtn.onclick = addPeach;
+
+var removeFirst = function() {
+  if (trees.length < 1) {
+    document.getElementById("error").innerHTML = "There aren't any trees in the list to remove. Try adding some.";
+  }
+  else {
+    trees.shift();
+    displayTrees();
+    document.getElementById("error").innerHTML = "";
+  }
+};
+var removeFirstBtn = document.getElementById("removeFirst");
+removeFirstBtn.onclick = removeFirst;
+
+var removeSecond = function() {
+  if (trees.length < 2) {
+    document.getElementById("error").innerHTML = "There isn't a second tree in the list to remove. Try adding some.";
+  }
+  else {
+    trees.splice(1, 1);
+    displayTrees();
+    document.getElementById("error").innerHTML = "";
+  }
+};
+var removeSecondBtn = document.getElementById("removeSecond");
+removeSecondBtn.onclick = removeSecond;
+
+var removeLast = function() {
+  if (trees.length < 1) {
+    document.getElementById("error").innerHTML = "There aren't any trees in the list to remove. Try adding some.";
+  }
+  else {
+    trees.pop();
+    displayTrees();
+    document.getElementById("error").innerHTML = "";
+  }
+};
+var removeLastBtn = document.getElementById("removeLast");
+removeLastBtn.onclick = removeLast;
 
 /*
 var  = function() {
