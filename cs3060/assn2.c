41fa5095fa3d80dbd9fb317de5197ce62e3f73ec
HeatherHyer <heather.hyer@familysearch.org>
2018-09-13 22:51:16 -0600

    css and assn2

--- cs3060/assn2.c
+++ cs3060/assn2.c
@@ -4,19 +4,27 @@
 
 int main() {
   pid_t p;
+  char *opts[4] = {"ls", "-l", "-a", NULL};
   printf("Start\n");
   p = fork(); //p for the parent is the process id, and p for the child is 0
+
   if (p < 0) {
     perror("Creating process");
     return -1;
   }
   if (p == 0) {
     printf("Child here\n");
+    execv("/bin/ls", opts);
+    perror("Cannot exec");
+    return 2;
   } else {
     printf("Parent here\n");
+    int ret;
+    wait(&ret);
+    printf("Child returned %d\n", WEXITSTATUS(ret));
   }
 
   printf("Hello %d\n", p);
-  
+
   return 0;
 }
