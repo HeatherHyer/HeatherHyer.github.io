b465c3fc3ab6cc081c2346b050655276a4329eaf
HeatherHyer <heather.hyer@familysearch.org>
2018-11-20 15:18:57 -0700

    works

--- dgm2760/14/js/main.js
+++ dgm2760/14/js/main.js
@@ -7,7 +7,7 @@ var cObj;
 var fObj;
 
 // GET THE CONDITIONS
-weatherConditions.open('GET', 'https://api.openweathermap.org/data/2.5/weather?zip=84653,us&appid=9e5934632d7f34dcb20cc9a5ea15543d', true);
+weatherConditions.open('GET', '//api.openweathermap.org/data/2.5/weather?zip=84653,us&appid=9e5934632d7f34dcb20cc9a5ea15543d&units=imperial', true);
 weatherConditions.responseType = 'text';
 weatherConditions.send(null);
 
@@ -15,8 +15,10 @@ weatherConditions.onload = function() {
     if (weatherConditions.status === 200){
         cObj = JSON.parse(weatherConditions.responseText);
         console.log(cObj);
-
-
+        document.getElementById('location').innerHTML = cObj.name;
+        document.getElementById('weather').innerHTML = cObj.weather[0].description;
+        document.getElementById('temperature').innerHTML = cObj.main.temp;
+        document.getElementById('desc').innerHTML = "Wind Speed: " + cObj.wind.speed;
     } //end if
 }; //end function
 
@@ -29,15 +31,41 @@ weatherConditions.onload = function() {
 
 
 
-// GET THE FORECARST
-//weatherForecast.open('', '', true);
-//weatherForecast.responseType = 'text';
-//weatherForecast.send();
+// GET THE FORECAST
+weatherForecast.open('GET', '//api.openweathermap.org/data/2.5/forecast?zip=84653,us&appid=9e5934632d7f34dcb20cc9a5ea15543d&units=imperial', true);
+weatherForecast.responseType = 'text';
+weatherForecast.send();
 
 weatherForecast.onload = function() {
 if (weatherForecast.status === 200){
 	fObj = JSON.parse(weatherForecast.responseText);
 	console.log(fObj);
 
+  var date_raw = fObj.list[0].dt_txt;
+  date_raw = date_raw.substring(5,11);
+  document.getElementById('r1c1').innerHTML = date_raw;
+  var iconCode = fObj.list[0].weather[0].icon;
+  var iconPath = "//openweathermap.org/img/w/" + iconCode + ".png";
+  document.getElementById('r1c2').src = iconPath;
+  document.getElementById('r1c3').innerHTML = fObj.list[0].main.temp_min + "&deg;";
+  document.getElementById('r1c4').innerHTML = fObj.list[0].main.temp_max + "&deg;";
+
+  var date_raw = fObj.list[8].dt_txt;
+  date_raw = date_raw.substring(5,11);
+  document.getElementById('r2c1').innerHTML = date_raw;
+  var iconCode = fObj.list[8].weather[0].icon;
+  var iconPath = "//openweathermap.org/img/w/" + iconCode + ".png";
+  document.getElementById('r2c2').src = iconPath;
+  document.getElementById('r2c3').innerHTML = fObj.list[8].main.temp_min + "&deg;";
+  document.getElementById('r2c4').innerHTML = fObj.list[8].main.temp_max + "&deg;";
+
+  var date_raw = fObj.list[16].dt_txt;
+  date_raw = date_raw.substring(5,11);
+  document.getElementById('r3c1').innerHTML = date_raw;
+  var iconCode = fObj.list[16].weather[0].icon;
+  var iconPath = "//openweathermap.org/img/w/" + iconCode + ".png";
+  document.getElementById('r3c2').src = iconPath;
+  document.getElementById('r3c3').innerHTML = fObj.list[16].main.temp_min + "&deg;";
+  document.getElementById('r3c4').innerHTML = fObj.list[16].main.temp_max + "&deg;";
 } //end if
 }; //end function
