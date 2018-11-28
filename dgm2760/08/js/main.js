d38f6b2c8fa129549db7a15c3292c60204851ee3
HeatherHyer <heather.hyer@familysearch.org>
2018-11-28 11:14:19 -0700

    changes to a8

--- dgm2760/08/js/main.js
+++ dgm2760/08/js/main.js
@@ -3,8 +3,23 @@
 "use strict()";
 
 var answer = {
+  question: "Who is buried in Grant's tomb?",
+  answerOne: "Tom",
+  answerTwo: "Grant",
+  answerThree: "Sally",
+  answerFour: "Jordan",
+  correct: this.answerTwo,
+
+  display: function() {
+    document.getElementById("question").innerHTML = this.question;
+    document.getElementById("answerOne").innerHTML = this.answerOne;
+    document.getElementById("answerTwo").innerHTML = this.answerTwo;
+    document.getElementById("answerThree").innerHTML = this.answerThree;
+    document.getElementById("answerFour").innerHTML = this.answerFour;
+  },
+
   check: function(a) {
-    if (a == 2) {document.getElementById("message").innerHTML = "Correct!";}
+    if (a == this.correct) {document.getElementById("message").innerHTML = "Correct!";}
     else {document.getElementById("message").innerHTML = "Try Again";}
   }
 };
